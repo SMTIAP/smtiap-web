a9d858b568a01afc88b73ad8c06c20743fce8bc9
Finusha <finu0313@gmail.com>
2026-03-17 14:10:21 +0530

    Add ReviewAndPublish page for survey preview and publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OrganizationAdmin from './pages/OrganizationAdmin';
 import CreatedSurveys from './pages/CreatedSurveys';
-import SearchTemplate from './pages/SearchTemplate'; 
+import SearchTemplate from './pages/SearchTemplate';
 import CreateNewSurvey from './pages/CreateNewSurvey';
-import AddQuestions from './pages/AddQuestions'; 
+import AddQuestions from './pages/AddQuestions'; // Make sure this file name is exact!
+import ReviewAndPublish from './pages/ReviewAndPublish';
 
 export default function App() {
   return (
@@ -11,10 +12,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<OrganizationAdmin />} />
         <Route path="/created-surveys" element={<CreatedSurveys />} />
-        <Route path="/create-new" element={<div>Create New Survey Page</div>} />
         <Route path="/templates" element={<SearchTemplate />} />
+        
+        {/* Survey Creation Flow */}
         <Route path="/create-new-survey" element={<CreateNewSurvey />} />
         <Route path="/add-questions" element={<AddQuestions />} />
+        <Route path="/review-publish" element={<ReviewAndPublish />} />
       </Routes>
     </BrowserRouter>
   );
