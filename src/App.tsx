02a707ead82f10f8577d6b0be1b66c5409c6ba76
Muditha <89349527+muditha20@users.noreply.github.com>
2026-05-08 08:38:45 +0530

    apply auth protection to all internal route

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,16 @@ import Response from "./pages/Response";
 import Analytics from "./pages/Analytics";
 import RoleManagement from "./pages/RoleManagement";
 import Subscription from "./pages/Subscription";
-import PaymentSuccess from './pages/PaymentSuccess';
-import PaymentCancel from './pages/PaymentCancel';
+import PaymentSuccess from "./pages/PaymentSuccess";
+import PaymentCancel from "./pages/PaymentCancel";
 import Audit from "./pages/Audit";
 import NavBar from "./components/NavBar.tsx";
 import AuthPage from "./pages/AuthPage.tsx";
 import ResetPassword from "./pages/ResetPassword.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
-import ShareSurvey from './pages/ShareSurvey';
-import TakeSurvey from './pages/TakeSurvey';
-import SurveyResults from './pages/SurveyResults';
+import ShareSurvey from "./pages/ShareSurvey";
+import TakeSurvey from "./pages/TakeSurvey";
+import SurveyResults from "./pages/SurveyResults";
 
 function Layout() {
   return (
@@ -36,49 +36,148 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/take-survey/:surveyId" element={<TakeSurvey />} />
-                {/* Auth Routes */}
+        <Route path="/take-survey/:surveyId" element={<TakeSurvey />} />
+        {/* Auth Routes */}
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/take-survey/:surveyId" element={<TakeSurvey />} />
-        
+
         {/* Layout wrapper for all routes */}
         <Route element={<Layout />}>
-          {/* Dashboard & Survey Management */}
+          {/* Public Route */}
           <Route path="/" element={<LandingPage />} />
-           <Route
-      path="/admin"
-      element={
-        <ProtectedRoute>
-          <OrganizationAdmin />
-        </ProtectedRoute>
-      } />
-          
-          <Route path="/creator-dashboard" element={<CreatorDashboard />} />
-          <Route path="/created-surveys" element={<CreatedSurveys />} />
-          <Route path="/templates" element={<SearchTemplate />} />
-          <Route path="/subscription" element={<Subscription />} />
-          <Route path="/subscription/success" element={<PaymentSuccess />} />
-          <Route path="/subscription/cancel" element={<PaymentCancel />} />
-          
 
-          {/* Survey Creation Flow */}
-          <Route path="/create-new-survey" element={<CreateNewSurvey />} />
-          <Route path="/add-questions" element={<AddQuestions />} />
-          <Route path="/review-publish" element={<ReviewAndPublish />} />
-          <Route path="/share-survey" element={<ShareSurvey />} />
-          <Route path="/survey-results/:surveyId" element={<SurveyResults />} />
-
-          {/* Analytics & Responses */}
-          <Route path="/response" element={<Response />} />
-          <Route path="/analytics" element={<Analytics />} />
-          {/* Role Management */}
-          <Route path="/role-management" element={<RoleManagement />} />
-          {/* Audit Management */}
-          <Route path="/audit-log" element={<Audit />} />
+          {/* Protected tenant routes */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <OrganizationAdmin />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/creator-dashboard"
+            element={
+              <ProtectedRoute>
+                <CreatorDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/created-surveys"
+            element={
+              <ProtectedRoute>
+                <CreatedSurveys />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/templates"
+            element={
+              <ProtectedRoute>
+                <SearchTemplate />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/subscription"
+            element={
+              <ProtectedRoute>
+                <Subscription />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/subscription/success"
+            element={
+              <ProtectedRoute>
+                <PaymentSuccess />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/subscription/cancel"
+            element={
+              <ProtectedRoute>
+                <PaymentCancel />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-new-survey"
+            element={
+              <ProtectedRoute>
+                <CreateNewSurvey />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/add-questions"
+            element={
+              <ProtectedRoute>
+                <AddQuestions />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/review-publish"
+            element={
+              <ProtectedRoute>
+                <ReviewAndPublish />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/share-survey"
+            element={
+              <ProtectedRoute>
+                <ShareSurvey />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/survey-results/:surveyId"
+            element={
+              <ProtectedRoute>
+                <SurveyResults />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/response"
+            element={
+              <ProtectedRoute>
+                <Response />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/analytics"
+            element={
+              <ProtectedRoute>
+                <Analytics />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/role-management"
+            element={
+              <ProtectedRoute>
+                <RoleManagement />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/audit-log"
+            element={
+              <ProtectedRoute>
+                <Audit />
+              </ProtectedRoute>
+            }
+          />
         </Route>
-        
       </Routes>
     </BrowserRouter>
   );
